e38219c4c5640f5f6d7e60b43201b56c4f6b0917
Stefan Ghetu <106436265+ursus161@users.noreply.github.com>
2026-03-23 00:50:32 +0200

    am facut scheletul ptr Message class

--- main.cpp
+++ main.cpp
@@ -7,16 +7,19 @@ using namespace std;
 int main(){
 
     cout<<BigInt(3)+BigInt(5)<<endl;
+
     cout<<RSAKey::modInv(BigInt(3),BigInt(5))<<endl;
+
     RSAKey key(BigInt(257), BigInt(263), "Stefan");
     key.generate();
     RSAEngine engine(&key);
     engine.benchmark(3);
 
+    cout<<"inainte de criptare " << BigInt(42)<<endl;
     BigInt criptat = engine.encrypt(BigInt(42));
-    cout<<criptat<<endl;
+    cout<<"criptat " << criptat<<endl;
     BigInt decriptat = engine.decrypt(criptat);
-    cout<< decriptat << endl;
+    cout<< "dupa decriptare " << decriptat << endl;
     cout << engine << endl;
 
     cout<<key.getOwner() << endl;
---
+++ message.h
@@ -0,0 +1,31 @@
+class Message { 
+
+
+    private:
+
+    char* text;
+    BigInt* blocks;
+    int blockCount;
+    bool isEncrypted;
+
+    public:
+
+        Message(const char* msj);
+        Message(const Message& msj);
+        Message& operator=(const Message& msj);
+
+        ~Message();
+
+        void toBlocks(Message& msj);
+
+        void fromBlocks(BigInt* blocks);
+
+       int getBlockCount() const;
+       bool getIsEncrypted() const;
+       BigInt* getBlocks() const;
+       const char* getText() const;
+
+      friend ostream& operator<<(ostream& os, const Message& msj);
+      friend istream& operator>>(istream& is,  Message& msj);
+
+};
